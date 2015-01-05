8db92db32c0bdb4a4641dae98d21eef8b7d20d90
AlexeyKlochkov <klochkov.ru@gmail.com>
2015-01-05 15:14:10 -0800

    SL-9

--- nav1.php
+++ nav1.php
@@ -43,6 +43,7 @@ foreach ($items as $val) {
     $menu .= ">" . $val['label'] . "</a></li>";
 }
 $menu .= '
-    </ul><div id="Logout" style="left: 85%;position:absolute;"><span style="color: white;padding-top: 3px;">'.strtolower($_SESSION["system_username"]).' </span><a href="loggedout.php"><img src="images/logout.ico"></a></div></div>';
+    </ul><div id="Logout" style="left: 85%;position:absolute;bottom:20%;"><span style="color: white;">'.strtolower($_SESSION["system_username"]).' </span>
+    <a href="loggedout.php"><img src="images/logout.ico" alt="Log out"></a></div></div>';
 echo $menu;
 ?>
