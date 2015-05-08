7cc4c4e9fb58853a63b595a7be50a46cdf0ea34b
AlexeyKlochkov <klochkov.ru@gmail.com>
2015-05-08 11:30:25 -0700

    small bug fix

--- wif_list.php
+++ wif_list.php
@@ -96,21 +96,11 @@ $wif_table .= "</table>";
 <link href='js/jquery-ui-1.10.3.custom.min.css' rel='stylesheet' type='text/css' />
 
 <title>WIF List</title>
-<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script> 
-<script type="text/javascript" src="js/jquery.validate.js"></script> 
+<script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
 <script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 <script>
   $(document).ready(function(){
-    
-	$( "#wif_form" ).validate({
-  rules: {
-    task_rate: {
-      required: false,
-      number: true
-    }
-  }
-});
-	
+
 	$( "#wif_status_select" ).change(function() {
 	  if ($( "#wif_status_select" ).val()==2){
 		 //alert( "Handler for .change() called." );
